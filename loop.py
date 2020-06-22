da4d98305703f9d8f297cba8eca82eb2e7f86719
sachinlalms <sachinlalms95@gmail.com>
2020-06-22 13:46:29 +0530

    loop2

--- loop.py
+++ loop.py
@@ -1,6 +1,29 @@
-n = 1
-while n < 10:
-    print(n)
-    n = n+1
-else :
-    print("Print 10 Natural")
+#While_loop
+#
+# x = 1
+# while x < 10:
+#     print(x)
+#     x = x+1
+# else :
+#     print("Print 10 Natural")
+
+# #For_loop
+# files=["jocker.mp4","memories.mp4","money heist.mp4"]
+# for x in files:
+#
+#     print(x[0:-4])
+
+# for x in range(10):
+# #     print(x+1)
+# for x in range(20,30,5):
+#     print(x)
+
+# Miltiplication Table
+print("Multiplication Table")
+print("")
+n=int(input("Enter the Multiant Number:"))
+print(n)
+l=int(input("Enter the limit:"))
+for m in range(1 , l+1):
+
+    print(m,"*",n,"=",m*n)
